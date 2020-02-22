397bb796cc928cedab28b2bbbf52ab67d8aeeafd
Jakub Findura <jakub.findura@r-das.sk>
2020-02-22 15:06:13 +0100

    Add CoAPtain HostedService

---
+++ CoAPtain/CoAPtainHostedService.cs
@@ -0,0 +1,61 @@
+﻿/*
+ * MIT License
+ * 
+ * Copyright(c) 2020 Jakub Findura
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ */
+
+using Com.AugustCellars.CoAP.Server;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CoAPtain
+{
+    /// <summary>
+    /// Hosted service for CoAP server
+    /// </summary>
+    public class CoAPtainHostedService : IHostedService
+    {
+        private readonly ILogger<CoAPtainHostedService> logger;
+
+        private CoapServer server;
+
+        /// <summary>
+        ///Initializes a instance of <see cref="CoAPtainHostedService"/>.
+        /// </summary>
+        public CoAPtainHostedService(ILogger<CoAPtainHostedService> logger)
+        {
+            this.logger = logger;
+        }
+
+        /// <inheritdoc/>
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Starting CoAP host service");
+
+            server = new CoapServer();
+
+            server.Start();
+
+            return Task.CompletedTask;
+        }
+
+        /// <inheritdoc/>
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Stopping CoAP host service");
+
+            server.Stop();
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ CoAPtain/DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * MIT License
+ * 
+ * Copyright(c) 2020 Jakub Findura
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ */
+
+using CoAPtain;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// Extensions for <see cref="IServiceCollection"/> interface.
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+
+        /// <summary>
+        /// Register CoAPtain to <see cref="IServiceCollection"/>
+        /// </summary>
+        public static IServiceCollection AddCoAPtain(this IServiceCollection services)
+        {
+            services.AddHostedService<CoAPtainHostedService>();
+
+            return services;
+        }
+    }
+}
